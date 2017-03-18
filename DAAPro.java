f16c0aab0b51929053a767406ae8711db81dff33
thusith94 <tkumarage20@gmail.com>
2017-03-18 10:03:54 +0530

    Add files via upload

---
+++ DAAPro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daapro;
+
+/**
+ *
+ * @author Dilhan
+ */
+public class DAAPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            MainWindow main=new MainWindow();
+            main.setVisible(true);
+           
+    }
+
+}
+
